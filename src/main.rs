34c5e7a6097c0e2954b29e75f6ec272223d20641
Abraao Neves <6154363+absnows@users.noreply.github.com>
2023-07-30 23:20:32 -0300

    Add test for routers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+use crate::routers::content_birth_routers::ContentBirthRouter;
+
+pub mod routers;
+
+#[get("/")]
+async fn get() -> impl Responder {
+    ContentBirthRouter::get_router().await;
+    HttpResponse::Ok().body("Hello from this get")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Call function from get resource external
+    ContentBirthRouter::get_router().await;
+
+    HttpServer::new(|| App::new().service(get))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
---
+++ src/routers/content_birth_routers.rs
@@ -0,0 +1,8 @@
+#[derive(Debug)]
+pub struct ContentBirthRouter {}
+
+impl ContentBirthRouter {
+    pub async fn get_router() {
+        println!("Hello from get here on ContentBirthRouter");
+    }
+}
