9beb0fc791effb932e03394713952cf74688d048
Abraao Neves <6154363+absnows@users.noreply.github.com>
2023-08-05 09:09:13 -0300

    Cache local search by date

--- src/routers/content_birth_routers.rs
+++ src/routers/content_birth_routers.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, HttpResponse, Responder};
+use redis::Commands;
 
 #[derive(Debug)]
 pub struct ContentBirthRouter {}
@@ -6,7 +7,16 @@ pub struct ContentBirthRouter {}
 impl ContentBirthRouter {
     pub async fn get(path: web::Path<String>) -> impl Responder {
         let birthdate = path.into_inner();
-        dbg!(&birthdate);
-        HttpResponse::Ok().body("Get from here...")
+        let redis_url = "redis://127.0.0.1/";
+        let client = redis::Client::open(redis_url).expect("File to open client configuration");
+        let mut connection = client
+            .get_connection()
+            .expect("Failed to generate connection ");
+
+        let content: String = connection
+            .get(birthdate)
+            .expect("Failed to search date value");
+
+        HttpResponse::Ok().body(content)
     }
 }
