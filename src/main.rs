7091e823fd8d8912d0b8f2f18bd1e2ae047f7d7e
Abraao Neves <6154363+absnows@users.noreply.github.com>
2023-08-05 15:57:19 -0300

    Improve server configuration

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
 use std::env;
 
-use crate::routers::content_birth_routers::ContentBirthRouter;
-use crate::routers::ping_controller::PingController;
+pub mod app;
 
-use actix_web::{middleware::Logger, web, App, HttpServer};
-
-mod routers;
-
-//TODO: Build a route to get information with Birth Date
-//TODO: Search information from Key Value datababse
-//TODO: Build fix prompt by country and language
-//TODO: Integrate with some LLVM
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+fn main() {
     // environment log configration
     dotenv::dotenv().ok();
 
@@ -23,20 +12,5 @@ async fn main() -> std::io::Result<()> {
 
     env_logger::init();
 
-    let address = env::var("BIND_ADDRESS").expect("Any address was configured");
-
-    HttpServer::new(|| App::new().wrap(Logger::default()).configure(routes))
-        .bind(&address)?
-        .run()
-        .await
-}
-
-fn routes(app: &mut web::ServiceConfig) {
-    app.service(
-        web::scope("/api")
-            .service(
-                web::resource("/dates/{birthdate}").route(web::get().to(ContentBirthRouter::get)),
-            )
-            .service(web::resource("/ping").route(web::get().to(PingController::pong))),
-    );
+    let _ = app::start();
 }
